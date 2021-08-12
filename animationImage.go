8918f3e5446f796b8224265902a3cabdbc690c4d
Jermination <bigchief64@yahoo.com>
2021-08-12 10:13:28 -0500

    animation working

--- animationImage.go
+++ animationImage.go
@@ -11,11 +11,26 @@ type Anim struct {
 	image     *ebiten.Image
 	baseImage *ebiten.Image
 	tools.Box
-	speed, row int
+	speed, row, frameCount, column, maxColumns, maxRows int
 }
 
 func (a *Anim) Draw() (*ebiten.Image, int, int) {
 	return a.image, a.X, a.Y
+}
+
+func (a *Anim) Update(){
+	a.frameCount++
+
+	if a.frameCount >= a.speed {
+		a.frameCount = 0
+		if a.column >= a.maxColumns{a.column = 0}
+		
+		x := a.column*a.W
+		y := a.row * a.H
+		a.image = a.baseImage.SubImage(image.Rect(x, y, x + a.W, y + a.H)).(*ebiten.Image)
+
+		a.column++
+	}
 }
 
 func NewAnim(x, y int, im *ebiten.Image) *Anim {
@@ -25,6 +40,11 @@ func NewAnim(x, y int, im *ebiten.Image) *Anim {
 	a.Y = y
 	a.W = 32
 	a.H = 32
+	a.speed = 5
+	
+	w, h := im.Size()
+	a.maxColumns = w/a.W
+	a.maxRows = h/a.H
 
 	a.image = a.baseImage.SubImage(image.Rect(0, 0, a.W, a.H)).(*ebiten.Image)
 
